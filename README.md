e4a644eb44e2be5fbe8bc2f122c626a0c3ac536e
Omkar Kulkarni <omkisaik@gmail.com>
2026-03-07 14:21:55 -0800

    Initial DB setup

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # STA141B.FinalProject
 
 Research Question: To what extent has the growth of renewable energy generation (wind/solar) actually reduced carbon emissions across different U.S. states over a [timeframe]?
+
+
+Sources/Docs:
+    - [documentation](https://www.sqlitetutorial.net/sqlite-python/)
---
+++ initialize_db.py
@@ -0,0 +1,55 @@
+"""
+This script initializes the SQLite database and creates the necessary tables for the project.
+All workflow follows the official sqllite3 documentation and lecture files
+
+Note: In order to connect to the database from the command-line, do sqlite3 Database/Energy_Data.db
+"""
+
+import sqlite3 as db
+
+# create tables for different data sources/sets
+
+create_tables = [
+    """CREATE TABLE IF NOT EXISTS renewable_energy 
+        (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            state TEXT NOT NULL,
+            date DATE NOT NULL,
+            renewable_energy_type TEXT NOT NULL,
+            renewable_energy_output INT NOT NULL
+        );
+    """,
+
+    """CREATE TABLE IF NOT EXISTS carbon_emissions
+        (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            state TEXT NOT NULL,
+            date DATE NOT NULL,
+            carbon_emissions_type TEXT NOT NULL,
+            carbon_emissions_value REAL NOT NULL
+        );
+    """,
+
+    """CREATE TABLE IF NOT EXISTS state_policy
+        (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            state TEXT NOT NULL,
+            date DATE NOT NULL,
+            policy_type TEXT NOT NULL,
+            policy_summary REAL NOT NULL
+        );
+    """ 
+]
+
+# connect to database or create it if it doesn't exist and create tables
+try:
+    with db.connect('Database/Energy_Data.db') as conn:
+        print(f"SQLite database with version {db.sqlite_version} connected successfully. ")
+        cursor = conn.cursor()
+        for table in create_tables:
+            cursor.execute(table)
+        conn.commit()
+        print("Tables created successfully.")
+
+except db.OperationalError as e:
+    print(f"SQLite database connection failed: {e}")
