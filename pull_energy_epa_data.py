2d0280038c8dc1f1551ef2ea348ead691c7be7b8
CalvinADorsey <cadorsey@ucdavis.edu>
2026-03-13 22:33:08 -0700

    Add files via upload

--- pull_energy_epa_data.py
+++ pull_energy_epa_data.py
@@ -1,26 +1,160 @@
-from dotenv import load_dotenv
-import os
-import requests
 import pandas as pd
+import requests
+import json
 
-load_dotenv() # load the .env file
-EIA_API_KEY = os.getenv("EIA_API_KEY")
 
-url = f"https://api.eia.gov/v2/seds/data?api_key={EIA_API_KEY}"
+# read your api key from file
 
-params = {
-    "frequency": "annual",
-    "data[]": "value",
-    "start": "1970",
-    "end": "2024",
-    "sort[0][column]": "period",
-    "sort[0][direction]": "desc",
-    "offset": 0,
-    "length": 5000
-}
+def read_key(keyfile):
+    with open(keyfile) as f:
+        return f.readline().strip("\n")
+# fill with your api key location
+key = read_key("Desktop/eia_key.txt") 
 
-response = requests.get(url, params=params)
 
-print(response.status_code)
+# API pull for renewable energy
 
-print(response.json())
+def renewable_energy_pull():
+    all_rows = []
+    offset = 0
+    renewables_url = f"https://api.eia.gov/v2/electricity/electric-power-operational-data/data/?api_key={key}"
+    while True:
+        params = {
+            "frequency": "annual",
+            "data": [
+                "generation"
+            ],
+            "facets": {
+                "fueltypeid": [
+                    "AOR",
+                    "REN",
+                    "SPV",
+                    "STH",
+                    "SUN",
+                    "WAS",
+                    "WND",
+                    "WNT"
+                ]
+            },
+            "start": "2001",
+            "end": "2010",
+            "sort": [
+                {
+                    "column": "period",
+                    "direction": "asc"
+                }
+            ],
+            "offset": offset,
+            "length": 5000
+        }
+
+        headers = {"X-Params": json.dumps(params)}
+
+        renewables_response = requests.get(renewables_url, headers = headers)
+
+        rows = renewables_response.json()["response"]["data"]
+        all_rows.extend(rows)
+        offset += 5000
+        if len(rows) < 5000:
+            break
+
+    # conversion of renewable energy data to data frame
+
+    renewable_energy = pd.DataFrame(all_rows)
+
+    renewable_energy["generation"] = pd.to_numeric(renewable_energy["generation"], errors = "coerce")
+    renewable_energy["period"] = pd.to_numeric(renewable_energy["period"])
+
+    renewable_energy["generation"] = renewable_energy["generation"].fillna(0)
+    renewable_energy["period"] = renewable_energy["period"].fillna(0)
+
+    renewable_energy = renewable_energy.rename(columns={
+        "location": "state",
+        "period": "date",
+        "fuelTypeDescription": "renewable_energy_type",
+        "sectorDescription": "renewable_energy_sector",
+        "generation": "renewable_energy_output"
+    })
+
+    renewable_energy = renewable_energy[[
+        "state",
+        "date",
+        "renewable_energy_type",
+        "renewable_energy_sector",
+        "renewable_energy_output"
+    ]]
+
+    return renewable_energy
+    # original columns: 'period', 'location', 'stateDescription', 'sectorid', 'sectorDescription', 'fueltypeid', 'fuelTypeDescription', 'generation', 'generation-units'
+
+
+# API pull for carbon emissions from eia website
+
+def carbon_emissions_pull():
+    all_rows = []
+    offset = 0
+    emission_url = f"https://api.eia.gov/v2/co2-emissions/co2-emissions-aggregates/data/?api_key={key}"
+    while True:
+        params = {
+            "frequency": "annual",
+            "data": [
+                "value"
+            ],
+            "facets": {},
+            "start": "2001",
+            "end": "2010",
+            "sort": [
+                {
+                    "column": "period",
+                    "direction": "asc"
+                }
+            ],
+            "offset": offset,
+            "length": 5000
+        }
+        headers = {"X-Params": json.dumps(params)}
+
+        emission_response = requests.get(emission_url, headers = headers)
+
+        rows = emission_response.json()["response"]["data"]
+        all_rows.extend(rows)
+        offset += 5000
+        if len(rows) < 5000:
+            break
+
+    # conversion of carbon emission data to data frame
+
+    carbon_emissions = pd.DataFrame(all_rows)
+
+    # change numeric values from string to int
+    carbon_emissions["value"] = pd.to_numeric(carbon_emissions["value"], errors = "coerce")
+    carbon_emissions["period"] = pd.to_numeric(carbon_emissions["period"])
+    
+    # fill null values with 0
+    carbon_emissions["value"] = carbon_emissions["value"].fillna(0)
+    carbon_emissions["period"] = carbon_emissions["period"].fillna(0)
+
+    carbon_emissions = carbon_emissions.rename(columns={
+        "stateId": "state",
+        "period": "date",
+        "sector-name": "carbon_emissions_sector",
+        "value": "carbon_emissions_value"
+    })
+
+    carbon_emissions = carbon_emissions[[
+        "state",
+        "date",
+        "carbon_emissions_sector",
+        "carbon_emissions_value"
+    ]]
+
+    return carbon_emissions
+
+    #original columns: 'period', 'sectorId', 'sector-name', 'fuelId', 'fuel-name', 'stateId', 'state-name', 'value', 'value-units'
+
+
+# API pull for state policy
+
+def state_policy_pull():
+    policy_url = ""
+
