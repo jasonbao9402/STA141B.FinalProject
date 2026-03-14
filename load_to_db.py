d8e10d66a433047894c563802f9fc4c6250fc536
CalvinADorsey <cadorsey@ucdavis.edu>
2026-03-13 20:57:07 -0700

    Add files via upload

---
+++ load_to_db.py
@@ -0,0 +1,16 @@
+import sqlite3 as sql
+from extract_eia import renewable_energy_pull, carbon_emissions_pull
+
+# Create dataframes from extract_eia.py
+carbon_emissions = carbon_emissions_pull()
+renewable_energy = renewable_energy_pull()
+
+# Use dataframes to fill tables in database
+def load_table(df, table_name):
+    conn = sql.connect(".../Energy_Data.db")
+    df.to_sql(table_name, conn, if_exists = "append", index = False)
+    conn.close()
+
+load_table(carbon_emissions, "carbon_emissions")
+load_table(renewable_energy, "renewable_energy")
+
