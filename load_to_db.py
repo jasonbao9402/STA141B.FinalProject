1a02b50c61f08f3009eaa4aecc3ec9104973ed87
CalvinADorsey <cadorsey@ucdavis.edu>
2026-03-13 22:36:14 -0700

    updated names

--- load_to_db.py
+++ load_to_db.py
@@ -1,5 +1,5 @@
 import sqlite3 as sql
-from extract_eia import renewable_energy_pull, carbon_emissions_pull
+from pull_energy_epa_data import renewable_energy_pull, carbon_emissions_pull
 
 # Create dataframes from extract_eia.py
 carbon_emissions = carbon_emissions_pull()
@@ -7,7 +7,8 @@ renewable_energy = renewable_energy_pull()
 
 # Use dataframes to fill tables in database
 def load_table(df, table_name):
-    conn = sql.connect(".../Energy_Data.db")
+    # fill with your database location
+    conn = sql.connect("Downloads/Energy_Data.db")
     df.to_sql(table_name, conn, if_exists = "replace", index = False)
     conn.close()
 
